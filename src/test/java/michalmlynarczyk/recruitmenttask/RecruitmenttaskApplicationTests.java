57112ed5abf27572652f3a07b43721d056304625
Michal Mlynarczyk <michalmlynarczykk@gmail.com>
2022-11-17 16:45:58 +0100

    init

---
+++ src/test/java/michalmlynarczyk/recruitmenttask/RecruitmenttaskApplicationTests.java
@@ -0,0 +1,13 @@
+package michalmlynarczyk.recruitmenttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmenttaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
