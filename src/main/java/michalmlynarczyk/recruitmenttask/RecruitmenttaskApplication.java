680246e3beb9accf6511c948a827d91aa966b0d5
Michal Mlynarczyk <michalmlynarczykk@gmail.com>
2022-11-21 12:42:37 +0100

    add: couple beans

--- src/main/java/michalmlynarczyk/recruitmenttask/RecruitmenttaskApplication.java
+++ src/main/java/michalmlynarczyk/recruitmenttask/RecruitmenttaskApplication.java
@@ -1,13 +1,30 @@
 package michalmlynarczyk.recruitmenttask;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
+@EnableAsync
 public class RecruitmenttaskApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RecruitmenttaskApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(RecruitmenttaskApplication.class, args);
-	}
+    @Bean
+    public ThreadPoolTaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(15);
+        executor.setQueueCapacity(25);
+        return executor;
+    }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
